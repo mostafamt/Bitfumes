6da8b775d810e0f6cd56f57f83f6d729854866f4
Mostafa <mostafa@localhost.localdomain>
2020-07-10 17:25:03 +0200

    Factory and Seed

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -3,7 +3,9 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Model\Category::class, function (Faker $faker) {
+    $word = $faker->word;
     return [
-        //
+        'name' => $faker->word,
+        'slug' => str_slug($faker->word),
     ];
 });
