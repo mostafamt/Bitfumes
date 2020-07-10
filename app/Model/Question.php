7e656aee56bad63d4af7b1c3c4bd648b4f22b48d
Mostafa <mostafa@localhost.localdomain>
2020-07-10 22:30:04 +0200

    CRUD for Question along with Question Resource Transformation

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -7,7 +7,14 @@ use App\User;
 
 class Question extends Model
 {
-    //
+    protected $guarded = [];
+    
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+    
+
     public function user()
     {
         return $this->belongsTo(User::class);
@@ -22,4 +29,10 @@ class Question extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function getPathAttribute()
+    {
+        return asset("api/question/$this->slug");
+    }
+
 }
